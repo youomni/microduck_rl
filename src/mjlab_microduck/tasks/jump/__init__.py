0062bb0de3fdfe8ec8947cbdb98de2f4292652da
youomni <58968741+youomni@users.noreply.github.com>
2026-09-20 08:33:46 +0300

    Update __init__.py

--- src/mjlab_microduck/tasks/jump/__init__.py
+++ src/mjlab_microduck/tasks/jump/__init__.py
@@ -1,11 +1,20 @@
-import gymnasium as gym
-from mjlab_microduck.tasks.jump.jump_env_cfg import make_microduck_jump_env_cfg, MicroduckJumpRlCfg
+# Import jump env cfg
+from .microduck_jump_env_cfg import (
+    make_microduck_jump_env_cfg,
+    MicroduckJumpRlCfg,
+)
 
-gym.register(
-    id="Mjlab-Jump-Flat-MicroDuck",
-    entry_point="mjlab.envs:ManagerBasedRlEnvCfg",
-    kwargs={
-        "env_cfg_entry_point": make_microduck_jump_env_cfg,
-        "rsl_rl_cfg_entry_point": MicroduckJumpRlCfg,
-    },
+# Register Jump task
+register_mjlab_task(
+    task_id="Mjlab-Jump-Flat-MicroDuck",
+    env_cfg=make_microduck_jump_env_cfg(),
+    play_env_cfg=make_microduck_jump_env_cfg(play=True),
+    rl_cfg=MicroduckJumpRlCfg,
+    runner_cls=MicroduckOnPolicyRunner,
+)
+
+# Add Jump backlash task variant
+_BACKLASH_TASKS = (
+    # ... existing backlash tasks ...
+    ("Mjlab-Jump-Flat-Backlash-MicroDuck", make_microduck_jump_env_cfg, {}, MicroduckJumpRlCfg, _BL_GROUNDCONTACT),
 )
